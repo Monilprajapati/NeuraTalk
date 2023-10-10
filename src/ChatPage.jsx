3e46e6a53fc6222492484ad3a7dd1289bb339a06
Monil Prajapati <monillprajapatii@gmail.com>
2023-10-11 00:59:47 +0530

    Added message component for user and bot and made some chaneges into input field

--- src/ChatPage.jsx
+++ src/ChatPage.jsx
@@ -1,36 +1,69 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./component/Navbar";
 // import { Configuration, OpenAIApi } from "openai";
 import { TbSend } from "react-icons/tb";
 import Message from "./component/Message";
+import bot from "./assets/chatbot.png"
+import user from "./assets/user.png"
 
 const ChatPage = () => {
   const [prompt, setPrompt] = useState("");
+  const [gptResponse, setgptResponse] = useState("");
+  const [chatList, setChatList] = useState([
+    {
+      id: 1,
+      img: bot,
+      prompt: "Hello, How can i help you"
+    }
+  ])
   console.log(prompt);
 
-  const onSubmit = () => {
-    console.log(prompt);
+  // useEffect(() => {
+  //   console.log(chatList);
+  // }, [chatList])
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter" && prompt.length > 0) {
+      event.preventDefault();
+      onResponse();
+    }
   }
+  const onResponse = () => {
+    const newMessage = {
+      id: chatList.length + 1, // Generate a unique ID for each message
+      img: user,
+      prompt: prompt,
+    };
+
+    setChatList([...chatList, newMessage]);
+    setPrompt("");
+  };
   return (
-    <div className="w-screen h-screen flex justify-between flex-col gap-2">
+    <div className="w-screen h-screen flex justify-between flex-col">
       <Navbar />
       <div className="mx-4 md:mx-7 lg:mx-24 xl:mx-48">
-        <div className="chatArea">
-          <Message />
+        <div className="chatArea pb-3 pr-7 pl-2 flex gap-5 flex-col overflow-y-scroll h-[500px]">
+          {chatList.map((chat) => {
+            return <Message key={chat.id} img={chat.img} prompt={chat.prompt} />;
+          })}
         </div>
-        <div className="inputField flex items-center justify-between my-3 border border-black rounded-md relative">
-          <textarea
-            rows={1}
-            type="text"
-            placeholder="Type your message here"
-            className="mr-12 font-lato text-lg py-3 px-2 w-full bg-transparent outline-none resize-none"
-            onChange={(e) => setPrompt(e.target.value)}
-          />
-          {prompt.length > 0 && (
-            <button className="btn absolute right-0 mr-2 bg-blue-500 hover:bg-blue-600 p-2 rounded-md" onClick={() => setPrompt("")}>
-              <TbSend className="text-xl cursor-pointer" />
-            </button>
-          )}
+        <div className={`chatInput ${prompt.length > 95 ? "h-20" : "h-fit"}`}>
+          <div className="inputField flex h-full items-center justify-between my-3 border border-black rounded-md relative">
+            <textarea
+              rows={1}
+              type="text"
+              value={prompt}
+              placeholder="Type your message here"
+              className="mr-12 font-lato text-lg py-3 px-2 h-full w-full bg-transparent outline-none resize-none"
+              onChange={(e) => setPrompt(e.target.value)}
+              onKeyDown={handleKeyPress}
+            />
+            {prompt.length > 0 && (
+              <button className="btn absolute right-0 mr-2 bg-blue-500 hover:bg-blue-600 p-2 rounded-md" onClick={() => onResponse()}>
+                <TbSend className="text-xl cursor-pointer" />
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </div>
--- src/component/Message.jsx
+++ src/component/Message.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Message = ({img,prompt}) => {
+const Message = ({ img, prompt }) => {
   return (
-    <div className='flex'>
-      <div className='img'>
-        <img src={img} alt="" />
+    <div className='flex gap-1'>
+      <div className='img rounded-full rounded-br-none bg-blue-200 w-12 h-12 p-2 md:w-14 md:h-14'>
+        <img src={img} alt="" className='w-full h-full' />
       </div>
-      <div className="message">
+      <div className="message md:text-lg flex flex-wrap mt-5 py-1 px-2 font-lato border border-blue-100 w-fit rounded-md rounded-tl-none bg-blue-100">
         {prompt}
       </div>
     </div>
