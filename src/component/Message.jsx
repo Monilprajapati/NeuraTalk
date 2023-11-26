b072c525cc36c95ee89ed801f786e061a9a8b270
Monil Prajapati <monillprajapatii@gmail.com>
2023-11-26 11:30:13 +0530

    Commit

--- src/component/Message.jsx
+++ src/component/Message.jsx
@@ -6,9 +6,9 @@ const Message = ({ img, prompt }) => {
       <div className='img rounded-full rounded-br-none bg-blue-200 w-12 h-12 p-2 md:w-14 md:h-14'>
         <img src={img} alt="" className='w-full h-full' />
       </div>
-      <div className="message md:text-lg flex flex-wrap mt-5 py-1 px-3 font-lato border border-blue-100 w-fit rounded-md rounded-tl-none bg-blue-100">
+      {/* <div className="message md:text-lg flex flex-wrap mt-5 py-1 px-3 font-lato border border-blue-100 w-fit rounded-md rounded-tl-none bg-blue-100">
         {prompt}
-      </div>
+      </div> */}
     </div>
   )
 }
