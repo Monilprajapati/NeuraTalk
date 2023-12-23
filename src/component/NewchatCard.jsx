6c4f696a4cabb41c1994107a7530ce83fe21ea44
Monil Prajapati <monillprajapatii@gmail.com>
2023-12-23 19:43:29 +0530

    Changes in NewChatCard component

--- src/component/NewchatCard.jsx
+++ src/component/NewchatCard.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+// NewchatCard.js
+import React from 'react';
 import { BiMessage } from "react-icons/bi";
+import { useChatContext } from '../context/ChatContext';
 
+const NewchatCard = ({ chatId, title, sidebar, setSidebar }) => {
+  const { setActiveChat } = useChatContext();
 
-const NewchatCard = ({ title, sidebar, setSidebar }) => {
-    return (
-        <div className="chat flex gap-3 items-center py-3 mx-5 my-3 pl-5 border border-black rounded-md" onClick={() => setSidebar(!sidebar)}>
-            <BiMessage className="text-xl text-black" />
-            <span className="text-black text-2xl font-lato">{title.length > 0
-                ? title && title.substr(0, 17) + "..."
-                : "New Chat"}</span>
-        </div>
-    )
-}
+  const handleChatClick = () => {
+    setActiveChat(chatId);
+    setSidebar(!sidebar);
+  };
 
-export default NewchatCard
+  return (
+    <div className="chat flex gap-3 items-center py-3 mx-5 my-3 pl-5 border border-black rounded-md" onClick={handleChatClick}>
+      <BiMessage className="text-xl text-black" />
+      <span className="text-black text-2xl font-lato">
+        {title.length > 0 ? title.substr(0, 17) + "..." : "New Chat"}
+      </span>
+    </div>
+  );
+};
+
+export default NewchatCard;
